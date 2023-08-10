8cdc64d91527bc861c7a6a70f2d78c7cc46e0b99
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-10 07:33:54 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: it is 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
-		return(0):
+	puts("\"Programming is like building a multilingual puzzle");
+		return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+/**
+ * main - the main function
+ * Return: it is 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
