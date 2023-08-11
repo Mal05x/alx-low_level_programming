cca5fd4deea4c47f9fd4489d191687c7a02d3f4f
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-11 10:42:15 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int j;
+
+	for (j = 0; j < 26; j++)
+	{
+		int i;
+
+		if (i == 5 && i == 17)
+			continue;
+		putchar(alphabet[j]);
+	}
+	putchar('\n');
+	return (0);
+}
