faab2d7acb9fe2134af087a40bea616c6e0927fb
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-14 13:20:50 +0100

    cfg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,18 @@
  * Return: 0
  */
 int main(void)
-{ 
+{
 	int i, j; 
- 
-    for (i = 0; i <= 89; i++)
-    { 
+
+	for (i = 0; i <= 89; i++)
+    {
 	    for (j = 0; j <= 89; j++)
-	    { 
-	       	if (i != j)
-		{ // check if digits are different
-		       	putchar(i + '0');
-		    putchar(j + '0');
-		    putchar(',' + ' ');
+	    {
+		    if (i != j)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',' + ' ');
 		}
 	    }
     }
