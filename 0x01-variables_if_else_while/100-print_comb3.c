66eb2004e78860c65015ea460780d22929b7f271
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-14 16:38:21 +0100

    jj

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * Return: 0
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, j;
-	j = 0;
+	int digit1, digit2;
 
-	for (i = 0; i <= 89; i++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-			if (i <= 9)
-			{
-				putchar((j + i) + '0');
-			}
-			else
-			{
-				putchar(i + '0');
-				putchar(',');
-				putchar(' ');
-			}
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
 
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - the main function
- * Return: always 0
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (i >= 9)
+		putchar((num % 10) + '0');
+		if (num == 9)
 			continue;
-
-
-		else
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
+		
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
+
 	return (0);
 }
