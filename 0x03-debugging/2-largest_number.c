0906629f033f36e64218a371659dca912c577c1d
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-18 09:07:55 -0700

    debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,10 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b && b > a)
 	{
 		largest = c;
 	}
-	
+	else
+	{
+		largest = (a == b == c);
+	}	
 	return (largest);
 }
