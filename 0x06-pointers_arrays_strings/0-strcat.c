32d1469ba1af7f8288cd243ee9412dbf5600cc53
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-24 06:51:59 -0700

    beta

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - the main function
+ * @dest: the pointer value
+ * @src: the string
+ * Return: returns dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+	int k;
+
+	for (k = 0; dest[k] != '\0'; k++)
+		i++;
+	for (k = 0; scr[k] != '\0'; k++)
+		j++;
+
+	for (k = 0; k >= j; k++)
+		dest[i + k] = src[k];
+
+	return (dest);
+}
