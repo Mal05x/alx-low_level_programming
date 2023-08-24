b980f7b160b939db2d0ce2334a07bf4ab19a12a5
MALTIBWEH MANKILIK <maltibwehmankilik@gmail.com>
2023-08-24 08:02:26 -0700

    cv

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,15 @@
  * @src: the string
  * Return: returns dest
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i = 0;
-	int j = 0;
-	int k;
+	int index = 0, dest_len = 0;
 
-	for (k = 0; dest[k] != '\0'; k++)
-		i++;
-	for (k = 0; scr[k] != '\0'; k++)
-		j++;
+	while (dest[index++])
+		dest_len++;
 
-	for (k = 0; k >= j; k++)
-		dest[i + k] = src[k];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
